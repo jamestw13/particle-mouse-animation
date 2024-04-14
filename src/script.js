8726b677aa0f4d43e8a9d0ac1cf8b3192f3e305a
TJ James <jamestw13@gmail.com>
2024-04-14 00:26:38 -0500

    added displacement canvas for raycasting

--- src/script.js
+++ src/script.js
@@ -75,12 +75,12 @@ const displacement = {};
 displacement.canvas = document.createElement('canvas');
 displacement.canvas.width = 80;
 displacement.canvas.height = 80;
-displacement.canvas.style.position = 'fixed';
-displacement.canvas.style.width = '256px';
-displacement.canvas.style.height = '256px';
-displacement.canvas.style.top = 0;
-displacement.canvas.style.left = 0;
-displacement.canvas.style.zIndex = 10;
+// displacement.canvas.style.position = 'fixed';
+// displacement.canvas.style.width = '256px';
+// displacement.canvas.style.height = '256px';
+// displacement.canvas.style.top = 0;
+// displacement.canvas.style.left = 0;
+// displacement.canvas.style.zIndex = 10;
 
 document.body.appendChild(displacement.canvas);
 
@@ -97,6 +97,7 @@ displacement.interactivePlane = new THREE.Mesh(
   new THREE.PlaneGeometry(10, 10),
   new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 );
+displacement.interactivePlane.visible = false;
 scene.add(displacement.interactivePlane);
 
 // Raycaster
@@ -104,6 +105,8 @@ displacement.raycaster = new THREE.Raycaster();
 
 // Coordinates
 displacement.screenCursor = new THREE.Vector2(9999, 9999);
+displacement.canvasCursor = new THREE.Vector2(9999, 9999);
+
 window.addEventListener('pointermove', event => {
   displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1;
   displacement.screenCursor.y = -(event.clientY / sizes.height) * 2 + 1;
@@ -134,6 +137,32 @@ const tick = () => {
 
   // Raycaster
   displacement.raycaster.setFromCamera(displacement.screenCursor, camera);
+  const intersections = displacement.raycaster.intersectObject(displacement.interactivePlane);
+  if (intersections.length) {
+    const uv = intersections[0].uv;
+
+    displacement.canvasCursor.x = uv.x * displacement.canvas.width;
+    displacement.canvasCursor.y = (1 - uv.y) * displacement.canvas.height;
+  }
+
+  /**
+   * Displacement
+   */
+  displacement.context.globalCompositeOperation = 'source-over';
+  displacement.context.globalAlpha = 0.02;
+  displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height);
+
+  // Draw glow
+  const glowSize = displacement.canvas.width * 0.25;
+  displacement.context.globalCompositeOperation = 'lighten';
+  displacement.context.globalAlpha = 1;
+  displacement.context.drawImage(
+    displacement.glowImage,
+    displacement.canvasCursor.x - glowSize * 0.5,
+    displacement.canvasCursor.y - glowSize * 0.5,
+    glowSize,
+    glowSize
+  );
 
   // Render
   renderer.render(scene, camera);
